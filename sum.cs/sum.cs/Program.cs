83dfe2751927b31f4d8ca4a3588285adb5dafb1d
pijush-patra <ppijush20@gmail.com>
2022-01-21 14:39:48 +0530

    initial commit

---
+++ sum.cs/sum.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 9;
+            int sum = a + b;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
